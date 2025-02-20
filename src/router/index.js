213cc02d73554e4318591a9ee208935a5fa6b5e0
afonso247 <afonso.h2002@gmail.com>
2025-02-20 09:48:13 -0300

    feat: confirguração inicial do projeto

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/LoginView.vue'
+import BooksView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/login'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      component: LoginView
     },
+    {
+      path: '/register',
+      component: RegisterView
+    },
+    {
+      path: '/books',
+      component: BooksView,
+      meta: { requiresAuth: true }
+    }
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !localStorage.getItem('token')) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
